96b71f5c9b9137f3c6becaec1b68e4b2f29f301e
Katelyn <katelynjewel@gmail.com>
2021-09-20 17:14:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x <= 400){
+    return(`This one is on me!`);
+   }  else if (x > 2500){
+    return(`No can do.`);
+   } else if (x >= 2000){
+    return(`I will gladly take your thirty bucks.`);
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  city === 'NYC' ? (`Ok, sounds good.`) : (`No go.`);
+  return city === 'NYC' ? (`Ok, sounds good.`) : (`No go.`);
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  switch(money){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
+
+  
